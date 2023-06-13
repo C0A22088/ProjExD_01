990a61dd9c855a8744df57ba05892b26837c6aff
sunago tomoya <c0a2208838@edu.teu.ac.jp>
2023-06-13 16:52:13 +0900

    演習

--- pygame_image.py
+++ pygame_image.py
@@ -1,18 +1,18 @@
-import sys
 import pygame as pg
-
+import sys
 
 def main():
     pg.display.set_caption("はばたけ！こうかとん")
     screen = pg.display.set_mode((800, 600))
     clock  = pg.time.Clock()
     bg_img = pg.image.load("ex01/fig/pg_bg.jpg")
-    kk_img = pg.image.load("ex01/fig/3.png") 
+    bg_imags = pg.transform.flip(bg_img, True, False)
+    kk_img = pg.image.load("ex01/fig/3.png")
     kk_img = pg.transform.flip(kk_img, True, False)
     kk_img2 = pg.transform.rotozoom(kk_img, 10, 1.0)
-    
+    kk_imgs = [kk_img, kk_img2]
     tmr = 0
-  
+
     while True:
         for event in pg.event.get():
             if event.type == pg.QUIT: return
@@ -20,7 +20,7 @@ def main():
         tmr += 1
         x = tmr%3200
         screen.blit(bg_img, [-x, 0])
-        screen.blit(kk_img2, [1600-x, 0])
+        screen.blit(bg_imags, [1600-x, 0])
         screen.blit(bg_img, [3200-x, 0])
         if x % 100 >= 50:
             screen.blit(kk_img, [300, 200])
@@ -29,11 +29,6 @@ def main():
 
         pg.display.update()
         clock.tick(100)
-
-
-
-
-
 
 
 if __name__ == "__main__":
