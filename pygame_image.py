d3dde09497924db595d802eafacf4eafeb56fe77
sunago tomoya <c0a2208838@edu.teu.ac.jp>
2023-06-13 16:26:53 +0900

    練習3

--- pygame_image.py
+++ pygame_image.py
@@ -7,8 +7,10 @@ def main():
     screen = pg.display.set_mode((800, 600))
     clock  = pg.time.Clock()
     bg_img = pg.image.load("ex01/fig/pg_bg.jpg")
-    kkbg_img = pg.image.load("ex01/fig/3.png") 
-    kkbg_img = pg.transform.flip(kkbg_img,True,False)
+    kk_img = pg.image.load("ex01/fig/3.png") 
+    kk_img = pg.transform.flip(kk_img, True, False)
+    kk_img2 = pg.transform.rotozoom(kk_img, 10, 1.0)
+    
     tmr = 0
     while True:
         for event in pg.event.get():
